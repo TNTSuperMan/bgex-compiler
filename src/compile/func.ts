bf6b5b563ff33a876b3a0757f8005d9225a06ba0
TNTSuperMan <tntsuperga@gmail.com>
2025-02-12 13:17:31 +0900

    変数を読む

--- src/compile/func.ts
+++ src/compile/func.ts
@@ -1,13 +1,21 @@
 import type { BGEXScope } from ".";
 import type { BGEXFunction } from "../parse/func";
+import { BGEXStatementType } from "../parse/statement";
 import { ptr2asm } from "./util";
-import { useVariable } from "./var";
+import { parseVariable, useVariable } from "./var";
 
 export const compileFunc = (scope: BGEXScope,token: BGEXFunction, name?: string) => {
     const n = name ?? token.name;
     const args = token.args.map(useVariable);
 
     scope.vars.push(new Map);
+    token.statements.forEach(e=>{
+        switch(e.type){
+            case BGEXStatementType.var:
+                e.vars.forEach(e=>scope.vars.at(-1)?.set(e.name, parseVariable(e)));
+                break;
+        }
+    })
     scope.vars.pop();
 
     return`:fn_${n}
