bdbe834f452be1bf8c4969460eff57a239055db7
TNTSuperMan <tntsuperga@gmail.com>
2025-02-12 14:24:35 +0900

    正規表現上手くいかないので手動に

--- src/compile/util.ts
+++ src/compile/util.ts
@@ -1,3 +1,6 @@
 export const ptr2asm = (ptr: number) => 
     `${Math.floor(ptr/256).toString(16)} ${(ptr%256).toString(16)}`
-export const escapeFunction = (path: string, name: string) => path.replaceAll(/(\/|\\|:)/,"_") + "_" + name;
+export const escapeFunction = (path: string, name: string) => path
+    .replaceAll("/", "_")
+    .replaceAll("\\","_")
+    .replaceAll(":", "_") + "_" + name;
