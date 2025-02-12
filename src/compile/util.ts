50399141fe58a1ed94cbafd19a0620dbd000ee6b
TNTSuperMan <tntsuperga@gmail.com>
2025-02-12 14:33:49 +0900

    padStartでアドレス桁合わせ

--- src/compile/util.ts
+++ src/compile/util.ts
@@ -1,5 +1,5 @@
 export const ptr2asm = (ptr: number) => 
-    `${Math.floor(ptr/256).toString(16)} ${(ptr%256).toString(16)}`
+    `${Math.floor(ptr/256).toString(16).padStart(2, "0")} ${(ptr%256).toString(16).padStart(2, "0")}`
 export const escapeFunction = (path: string, name: string) => path
     .replaceAll("/", "_")
     .replaceAll("\\","_")
