6897c2e8795862d5818da2f467f33493464d9d31
TNTSuperMan <tntsuperga@gmail.com>
2025-02-12 16:18:10 +0900

    適当に型のを作る

---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+declare function dumpkey(): number;
+declare function redraw(): void;
+declare function rect(x: number, y: number, w: number, h: number, color: number): void;
+declare function graph(id: number, x: number, y: number): void;
+declare function sound(id: number): void;
+declare function stopsound(): void;
+declare function io(id: 0 | 1 | 2 | 3): void;
