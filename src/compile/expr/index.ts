82a3384300de715193f28a8b606a3413a4e8741c
TNTSuperMan <tntsuperga@gmail.com>
2025-02-12 14:08:22 +0900

    コール部分の実装・関数も

---
+++ src/compile/expr/index.ts
@@ -0,0 +1,30 @@
+import type { BGEXScope } from "..";
+import { BGEXExpressionType, type BGEXExpression } from "../../parse/expr";
+
+const IOFunctionMap: {[key: string]: number|void} = { //関数と引数数のマップ
+    dumpkey: 0,
+    redraw: 0,
+    rect: 5,
+    graph: 3,
+    sound: 1,
+    stopsound: 0,
+    io: 1
+}
+
+export const compileExpression = (scope: BGEXScope, token: BGEXExpression): string => {
+    switch(token.type){
+        case BGEXExpressionType.call:
+            const arg = `/ ${token.args.map(e=>compileExpression(scope, e)).join(" ")}`;
+            const fn = scope.funcs.get(token.name);
+            if(fn){
+                if(IOFunctionMap[token.name] !== token.args.length) throw new Error("Argument length not match");
+                return `${arg} ${fn} call`;
+            }else if(IOFunctionMap[token.name]){
+                if(IOFunctionMap[token.name] !== token.args.length) throw new Error("Argument length not match");
+                return `${arg} ${token.name}`;
+            }else throw new Error("Not found function: " + token.name)
+            break;
+        default:
+            throw new Error("Not implemented");
+    }
+}
--- src/compile/func.ts
+++ src/compile/func.ts
@@ -1,6 +1,7 @@
 import type { BGEXScope } from ".";
 import type { BGEXFunction } from "../parse/func";
 import { BGEXStatementType } from "../parse/statement";
+import { compileExpression } from "./expr";
 import { ptr2asm } from "./util";
 import { parseVariable, useVariable } from "./var";
 
@@ -8,16 +9,23 @@ export const compileFunc = (scope: BGEXScope,token: BGEXFunction, name?: string)
     const n = name ?? token.name;
     const args = token.args.map(useVariable);
 
+    const exprs: string[] = [];
     scope.vars.push(new Map);
     token.statements.forEach(e=>{
         switch(e.type){
             case BGEXStatementType.var:
                 e.vars.forEach(e=>scope.vars.at(-1)?.set(e.name, parseVariable(e)));
                 break;
+            case BGEXStatementType.expr:
+                exprs.push(compileExpression(scope, e.expr));
+                break;
+            default:
+                throw new Error("not implemented")
         }
     })
     scope.vars.pop();
 
     return`:fn_${n}
-${args.map(e=>`${ptr2asm(e[2])}`)}`
+${args.map(e=>`${ptr2asm(e[2])}`)}
+${exprs.join("\n")}`
 }
