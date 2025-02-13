c6e2ec29b964da6e22f32dc0c25783a1262ea5f3
TNTSuperMan <tntsuperga@gmail.com>
2025-02-13 13:25:26 +0900

    比較演算子の実装

--- src/compile/expr/binary.ts
+++ src/compile/expr/binary.ts
@@ -4,7 +4,8 @@ import type { BGEXBinaryExpression } from "../../parse/expr/binary";
 
 const boolify = " 2 rem";
 const not = " 1 add";
-const and = (a: string, b: string) => `${a} ${boolify} ${b} ${boolify} add 1 greater`;
+const and = (a: string, b: string) => `${a}${boolify} ${b}${boolify} add 1 greater`;
+const or = (a: string, b: string) => `${a}${boolify} ${b}${boolify} add 0 greater`;
 
 export const compileBinaryExpression = (scope: BGEXScope, token: BGEXBinaryExpression): string => {
     const base = `${compileExpression(scope, token.left)} ${compileExpression(scope, token.right)} `;
@@ -17,6 +18,9 @@ export const compileBinaryExpression = (scope: BGEXScope, token: BGEXBinaryExpre
         case"==": case"===": return base + "equal" + boolify;
         case"!=": case"!==": return base + "equal" + not + boolify;
         case">": return base + "greater";
+        case">=":return or(base + "greater", base + "equal");
+        case"<": return base + "greater" + not + boolify;
+        case"<=":return or(base + "greater" + not, base + "equal");
         default: throw new Error("Not implemented binary expression: " + token.type)
     }
 }
