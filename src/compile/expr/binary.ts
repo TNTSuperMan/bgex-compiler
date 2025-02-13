d93a6d60044d25dd1d582f7d522a19ee4f475490
TNTSuperMan <tntsuperga@gmail.com>
2025-02-13 13:56:35 +0900

    ビット演算実装

--- src/compile/expr/binary.ts
+++ src/compile/expr/binary.ts
@@ -7,6 +7,15 @@ const not = " 1 add";
 const and = (a: string, b: string) => `${a}${boolify} ${b}${boolify} add 1 greater`;
 const or = (a: string, b: string) => `${a}${boolify} ${b}${boolify} add 0 greater`;
 
+const bitNot = (a: string) => `${a} ${a} nand`;
+const nand = (a:string,b:string) => `${a} ${b} nand`
+type BitKey = "and" | "or" | "xor"
+const bit: {[key in BitKey]: ((a: string, b: string) => string)} = {
+    and: (a,b) => bitNot(nand(a,b)),
+    or: (a,b) => nand(bitNot(a), bitNot(b)),
+    xor: (a,b) => nand(nand(a, nand(a, b)), nand(b, nand(a, b))) 
+}
+
 export const compileBinaryExpression = (scope: BGEXScope, token: BGEXBinaryExpression): string => {
     const base = `${compileExpression(scope, token.left)} ${compileExpression(scope, token.right)} `;
     switch(token.type){
@@ -21,6 +30,15 @@ export const compileBinaryExpression = (scope: BGEXScope, token: BGEXBinaryExpre
         case">=":return or(base + "greater", base + "equal");
         case"<": return base + "greater" + not + boolify;
         case"<=":return or(base + "greater" + not, base + "equal");
+        case"&": return bit.and(
+            compileExpression(scope, token.left),
+            compileExpression(scope, token.right));
+        case"|": return bit.or(
+            compileExpression(scope, token.left),
+            compileExpression(scope, token.right));
+        case"^": return bit.xor(
+            compileExpression(scope, token.left),
+            compileExpression(scope, token.right));
         default: throw new Error("Not implemented binary expression: " + token.type)
     }
 }
