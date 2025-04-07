da5474c1b90579a1c628608977d4d6a241a298ff
TNTSuperMan <abworld@outlook.jp>
2025-04-07 18:32:53 +0900

    コンパイル時エラーの時に発生ファイルを表示する

--- src/compile/index.ts
+++ src/compile/index.ts
@@ -15,51 +15,58 @@ export type BGEXScope = {
 }
 
 export const compileBGEX = (token: BGEXModule, exports: Map<string, Exports>): string => {
-    let asm = [`;@@ ${token.path}`];
-    const source = readFileSync(token.path).toString();
-    
-    const scope: BGEXScope = {
-        vars:[new Map],
-        funcs: new Map,
-        macro: token.macro,
-        path: token.path,
-        getAt: (at) => {
-            let l = 1;
-            let c = 1;
-            for(let i = 0;i < at;i++){
-                if(source[i] == "\n"){
-                    l++
-                    c = 1;
-                }else{
-                    c++
+    try{
+        let asm = [`;@@ ${token.path}`];
+        const source = readFileSync(token.path).toString();
+        
+        const scope: BGEXScope = {
+            vars:[new Map],
+            funcs: new Map,
+            macro: token.macro,
+            path: token.path,
+            getAt: (at) => {
+                let l = 1;
+                let c = 1;
+                for(let i = 0;i < at;i++){
+                    if(source[i] == "\n"){
+                        l++
+                        c = 1;
+                    }else{
+                        c++
+                    }
                 }
+                return [l,c];
             }
-            return [l,c];
-        }
-    };
-    
-    token.imports.forEach(e=>{
-        const m = exports.get(e.path);
-        if(!m) throw new ReferenceError("Not found module: " + e.path)
-        e.specifiers.forEach(t=>{
-            const res = m.find(e=>e[1] == t[0]);
-            if(!res) throw new ReferenceError("Not found token: " + t[0]);
-            if(res[0] == 0){
-                scope.funcs.set(res[1], res);
-            }else{
-                scope.vars[0]?.set(res[1], res[2]);
-            }
+        };
+        
+        token.imports.forEach(e=>{
+            const m = exports.get(e.path);
+            if(!m) throw new ReferenceError("Not found module: " + e.path)
+            e.specifiers.forEach(t=>{
+                const res = m.find(e=>e[1] == t[0]);
+                if(!res) throw new ReferenceError("Not found token: " + t[0]);
+                if(res[0] == 0){
+                    scope.funcs.set(res[1], res);
+                }else{
+                    scope.vars[0]?.set(res[1], res[2]);
+                }
+            })
         })
-    })
-
-    const vars: Variable[] = token.vars.map(e=>parseVariable(scope, e));
-    vars.forEach(e=>scope.vars[0]?.set(e[1], e));
-
-    const fnmap: [BGEXFunction, `${string}-${string}-${string}-${string}-${string}`][] = token.funcs.map(e=>[e, crypto.randomUUID()]);
-    fnmap.forEach(e=>scope.funcs.set(e[0].name, toExportiveFunction(e[0], e[1])))
-
-    asm.push(...fnmap.map(e => compileFunc(scope, e[0], escapeFunction(e[1], e[0].name))))
-    asm.push(...token.exportFunctions.map(e => compileFunc(scope, e, escapeFunction(token.path, e.name))))
-
-    return asm.join("\n");
+    
+        const vars: Variable[] = token.vars.map(e=>parseVariable(scope, e));
+        vars.forEach(e=>scope.vars[0]?.set(e[1], e));
+    
+        const fnmap: [BGEXFunction, `${string}-${string}-${string}-${string}-${string}`][] = token.funcs.map(e=>[e, crypto.randomUUID()]);
+        fnmap.forEach(e=>scope.funcs.set(e[0].name, toExportiveFunction(e[0], e[1])))
+    
+        asm.push(...fnmap.map(e => compileFunc(scope, e[0], escapeFunction(e[1], e[0].name))))
+        asm.push(...token.exportFunctions.map(e => compileFunc(scope, e, escapeFunction(token.path, e.name))))
+    
+        return asm.join("\n");
+    }catch(e){
+        if(e instanceof Error){
+            //console.error(e);
+            throw new Error(e.message + " at " + token.path, { cause: e });
+        }else throw e;
+    }
 }
